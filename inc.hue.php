21b5d947ae10a095a9470c129d147813548d5beb
kouala <kouala@gmail.com>
2015-02-18 20:46:26 +0100

    Update inc.hue.php

--- inc.hue.php
+++ inc.hue.php
@@ -3,9 +3,9 @@
 ------ Configuration
 -------------------------------------------- */
 // adresse IP du point d'accès Philips HUE
-$server 	= "88.182.182.3:33";
+$server 	= "MY-IP";
 // user : 10 à 40 caractères. Chiffres et lettres seulement
-$user		= "koualahomehue";
+$user		= "MY-NAME";
 // device : 1 à 40 caractères
-$device		= "zibase";
+$device		= "MY-DEVICE";
 ?>
